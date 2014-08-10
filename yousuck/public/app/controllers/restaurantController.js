acea85ee8657208b616e75ce730b9d99fadac3a4
Patrick Tsai <patosai@outlook.com>
2014-08-10 09:43:18 -0400

    Removed excess files

--- yousuck/public/app/controllers/restaurantController.js
+++
@@ -1,7 +0,0 @@
-app.controller("restaurantController", function($scope, $firebase){
-	var rootRef = new Firebase("https://intense-fire-7167.firebaseio.com/")
-	var restaurants = rootRef.child("restaurants");
-	var sync = $firebase(restaurants);
-
-	$scope.restaurants = sync.$asArray();
-})
