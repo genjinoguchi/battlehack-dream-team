69eeaedd80699e1d025d61175d6716ac01173568
Patrick Tsai <patosai@outlook.com>
2014-08-09 21:18:07 -0400

    Adding restaurant items + prices works

--- js/script.js
+++ js/script.js
@@ -1,11 +1,27 @@
 var venmo = {
-	user: patosai,
+	user: "patosai",
 	send: function(amount) {
 		window.open("https://venmo.com/" + venmo.user + "?txn=charge&amount=" + amount);
 	},
 }
 
-function getJsonData($scope, $http) {
-	$http({method: 'POST', url: 'restaurants/sample.json'}).success(function (data) {
-		$scope.dataFromJson = data; // response data
-	});
+var restaurant = {
+	foods:[],
+	add_food:function() {
+		var name = document.getElementById("add_food_name").value;
+		var price = document.getElementById("add_food_price").value;
+
+		var div = document.createElement("div");
+		div.id = "food";
+		div.innerHTML = name + "   " + price;
+
+		document.getElementById("add_food_name").value = "";
+		document.getElementById("add_food_price").value = "";
+
+		document.getElementById("main_wrapper").appendChild(div);
+
+		restaurant.foods.push(
+			{"name": name, "price": price}
+		);
+	}
+}
