57a24b8aa09a6a06d2e2ff40ea8d55db08517370
Patrick Tsai <patosai@outlook.com>
2014-08-10 04:46:59 -0400

    Fixed subtotals and payment link

--- js/module.js
+++ js/module.js
@@ -5,5 +5,21 @@ function mainController($scope, $http) {
 				$scope.dataFromJson = data; // response data
 			});
 	}
-	$scope.subtotal = 0.00;
+	$scope.subtotal = 0;
+	$scope.calculateSubtotal = function() {
+		$scope.subtotal = 0;
+		var div = document.getElementsByClassName("input");
+		for (var i = 0; i < div.length; i++) {
+			$scope.subtotal += div[i].value * parseFloat(div[i].name);
+		}
+		return $scope.subtotal;
+	}
+	$scope.calculateAndPaySubtotal = function() {
+		$scope.subtotal = 0;
+		var div = document.getElementsByClassName("input");
+		for (var i = 0; i < div.length; i++) {
+			$scope.subtotal += div[i].value * parseFloat(div[i].name);
+		}
+		venmo.send($scope.subtotal);
+	}
 }
