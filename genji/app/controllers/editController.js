6c504eabeff0401bc7539ab38d635859ebb9eccb
Genji Noguchi <genji.noguchi@gmail.com>
2014-08-10 08:22:57 -0400

    added the final product structure

---
+++ genji/app/controllers/editController.js
@@ -0,0 +1,4 @@
+app.controller("editController", function($scope, $firebase){
+	var rootRef = new Firebase("https://intense-fire-7167.firebaseio.com/")
+	
+})
---
+++ genji/app/controllers/restaurantController.js
@@ -0,0 +1,3 @@
+app.controller("restaurantController", function($scope, $firebase){
+	var rootRef = new Firebase("https://intense-fire-7167.firebaseio.com/")
+})
