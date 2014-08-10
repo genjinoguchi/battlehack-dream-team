885326466f9e101a3c2d16f958b82edecffba627
Genji Noguchi <genji.noguchi@gmail.com>
2014-08-10 11:03:46 -0400

    Enter twilio

--- app/public/app/controllers/editController.js
+++ app/public/app/controllers/editController.js
@@ -1,9 +1,9 @@
 app.controller("editController", function($scope, $firebase, $routeParams){
 	var rootRef = new Firebase("https://intense-fire-7167.firebaseio.com/restaurants/");
-	var orders = rootRef.child("-JTzTkaviRMl4PLRSFpB/orders");
+	var foods = rootRef.child("-JTzTkaviRMl4PLRSFpB/foods");
 	
-	var sync = $firebase(orders);
-	$scope.orders = sync.$asArray();
+	var sync = $firebase(foods);
+	$scope.menu = sync.$asArray();
 
 
 	
